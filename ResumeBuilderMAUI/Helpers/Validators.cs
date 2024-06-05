3b60cc6311acbd3946e9f6f8e744d08f9ad7e9ec
samet <sametcn99@gmail.com>
2024-06-05 22:48:29 +0300

    validators

---
+++ ResumeBuilderMAUI/Helpers/Validators.cs
@@ -0,0 +1,28 @@
+﻿namespace ResumeBuilderMAUI.Helpers
+{
+    internal class Validators
+    {
+        public static bool IsValidEmail(string email)
+        {
+            try
+            {
+                var addr = new System.Net.Mail.MailAddress(email);
+                return addr.Address == email;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        public static bool IsValidPhoneNumber(string phoneNumber)
+        {
+            return System.Text.RegularExpressions.Regex.Match(phoneNumber, @"^(\+[0-9]{9})$").Success;
+        }
+
+        public static bool IsValidUrl(string url)
+        {
+            return Uri.TryCreate(url, UriKind.Absolute, out Uri? uriResult) && uriResult != null;
+        }
+    }
+}
